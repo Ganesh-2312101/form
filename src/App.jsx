c3f81512d63a6e28d7d1852cfe058b216f44e8fd
Ganesh <mganesh1911@gmail.com>
2025-10-28 21:57:18 +0530

    Remove node_modules and add .gitignore

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import FormValidation from "./Formvalidation.jsx";
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>React Form Validation</h1>
+      <FormValidation />
+    </div>
+  );
+}
+
+export default App;
